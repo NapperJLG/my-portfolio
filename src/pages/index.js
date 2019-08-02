8680809e7df51b9197db262891121a0511e8d023
Jake Napper <napperjlg@gmail.com>
2019-08-02 22:39:10 +0100

    basic navbar added

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,17 @@
 import React from "react"
 import "../styles/global.css"
+import Navbar from "../components/navbar"
 
 const Index = () => {
-  return <h1>Hello world</h1>
+  return (
+    <div>
+      <Navbar />
+      <div>
+        <h1>Hi, I'm Jake</h1>
+        <h2>Front-end web developer and all around nerd</h2>
+      </div>
+    </div>
+  )
 }
 
 export default Index
